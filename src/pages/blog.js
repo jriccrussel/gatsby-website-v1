bf2216c18b8e452fc48029b3660450837741707d
bpoc-russeljeff <russel.jeff@bpoc.co.jp>
2021-03-07 17:40:47 +0800

    installing the env file, installing gatsby source file sytem & gatsby tranformer remark to generate and display the post and adding codes on the gatsby config and adding query code on the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Blog</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
 
-export default SecondPage
+  console.log(data);
+
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      
+      <ol>
+        {data.allMarkdownRemark.edges.map( (edge) => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export default BlogPage
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
   return (
     <Layout>
       <h1>Contact Page</h1>
-      <h2></h2>
+      {/* <h2></h2> */}
       <p>The best way to reach me <a href="https://github.com/jriccrussel" target="_blank" rel="noopener noreferrer">Contact me,</a> on github</p>
     </Layout>
   )
