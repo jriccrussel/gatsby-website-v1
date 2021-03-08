c2933b1b693c681918383af77d4f2d5f4a2ba5f6
bpoc-russeljeff <russel.jeff@bpoc.co.jp>
2021-03-08 16:24:13 +0800

    adding lines of code in the gatsby-node.js(no need to install)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+// Getting the slug
+exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === 'MarkdownRemark') {
+      const slug = path.basename(node.fileAbsolutePath, '.md')
+
+      createNodeField({
+        node,
+        name: 'slug',
+        value: slug,
+      })
+    // End createNodeField
+    } 
+    // End If node.internal.type
+  }
+// End exports onCreateNode
+
+
+// Dynamiclly create the slug pages and display in the templates/blog.js
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    // 1. Get path to template
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    // 2. Get markdown data
+    const res = await graphql(`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `)
+  
+    // 3. create new pages
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+      createPage({
+        component: blogTemplate,
+        path: `/blog/${edge.node.fields.slug}`,
+        context: {
+          slug: edge.node.fields.slug
+        }
+      })
+    })
+  }
